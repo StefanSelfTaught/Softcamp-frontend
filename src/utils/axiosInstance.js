460042c28a50a6c88e3d07bc0a99f1269fd5340e
pop stefan <stefanpop999@gmail.com>
2020-06-20 22:43:18 +0300

    deployment

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://softcamp-api.herokuapp.com/api/v1',
 });
 
 export default instance;
